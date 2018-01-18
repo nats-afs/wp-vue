1920a359b258f86df76638a8d187c616e2f69e3a
nats <nocturno1470@gmail.com>
2018-01-18 13:03:03 -0500

    deploy to firebase

--- src/components/forms/NewsForm.vue
+++ src/components/forms/NewsForm.vue
@@ -3,21 +3,23 @@
     <div class="row">
       <h1>Noticias</h1>
       <div class="divider"></div>
-      <form class="col s12">
+      <form @submit.prevent="saveNew" class="col s12">
         <div class="row">
           <div class="input-field col s12">
-            <input id="last_name" type="text" class="validate">
-            <label for="last_name">Titulo de la noticia</label>
+            <input id="title" type="text" class="validate" v-model="noticia.title">
+            <label for="title">Titulo de la noticia</label>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s12">
-            <textarea id="textarea1" class="materialize-textarea"></textarea>
+            <textarea id="textarea1" class="materialize-textarea" v-model="noticia.description"></textarea>
             <label for="textarea1">Descripcion</label>
           </div>
         </div>
         <div class="row">
-          
+           <button class="btn waves-effect waves-light" type="submit" name="action">GUARDAR
+            <i class="material-icons right">save</i>
+          </button>
         </div>
       </form>
     </div>  
@@ -25,7 +27,23 @@
 </template>
 <script>
 export default {
-  
+  data(){
+    return{
+      noticia:{
+        title: null,
+        description:null
+      }
+    }
+  },
+  methods:{
+    saveNew(){
+      var database = firebase.database();
+      
+      database.ref('news').push(this.noticia).then(()=>{
+        console.info('Noticia guardada!!');
+      });
+    }
+  }
 }
 </script>
 <style lang="scss">
